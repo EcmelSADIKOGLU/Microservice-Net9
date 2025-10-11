958e2aa84c04686360da5f24555272158c0c94a4
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2025-10-11 20:31:59 +0300

    ServiceResult

--- Microservice(Net9).Shared/ServiceResult.cs
+++ Microservice(Net9).Shared/ServiceResult.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Refit;
 using System.Net;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Microservice_Net9_.Shared
@@ -8,7 +10,7 @@ namespace Microservice_Net9_.Shared
     {
         [JsonIgnore] public HttpStatusCode StatusCode { get; set; }
 
-        public ProblemDetails? Fail { get; set; } //Başarılıysa null
+        public Microsoft.AspNetCore.Mvc.ProblemDetails? Fail { get; set; } //Başarılıysa null
 
 
         //lambda ile kullanırsan sadece get'i olan method'lar olur
@@ -29,14 +31,95 @@ namespace Microservice_Net9_.Shared
             return new ServiceResult
             {
                 StatusCode = HttpStatusCode.NotFound,
-                Fail = new ProblemDetails()
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
                 {
                     Title = "Not Found",
-                    Detail = "The request resource was not found"
+                    Detail = "The request resource was not found",
+                    Status = HttpStatusCode.NotFound.GetHashCode()
                 }
 
             };
 
+        }
+
+        public static ServiceResult Error(Microsoft.AspNetCore.Mvc.ProblemDetails problemDetails, HttpStatusCode statusCode)
+        {
+            return new ServiceResult()
+            {
+                StatusCode = statusCode,
+                Fail = problemDetails
+            };
+        }
+
+        public static ServiceResult Error(string title, string detail, HttpStatusCode statusCode)
+        {
+            return new ServiceResult()
+            {
+                StatusCode = statusCode,
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                {
+                    Title = title,
+                    Detail = detail,
+                    Status = statusCode.GetHashCode() //int karşılığı
+                }
+            };
+        }
+
+        public static ServiceResult Error(string title, HttpStatusCode statusCode)
+        {
+            return new ServiceResult()
+            {
+                StatusCode = statusCode,
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                {
+                    Title = title,
+                    Status = statusCode.GetHashCode() //int karşılığı
+                }
+            };
+        }
+
+        public static ServiceResult ErrorFromValidation(IDictionary<string, object?> errors)
+        {
+            return new ServiceResult()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                {
+                    Title = "Validation errors occured.",
+                    Detail = "Please check the errors property for more details",
+                    Extensions = errors,
+                    Status = HttpStatusCode.BadRequest.GetHashCode() //int karşılığı
+                }
+            };
+        }
+
+        public static ServiceResult ErrorFromProblemDetails(ApiException exception)
+        {
+            if (string.IsNullOrEmpty(exception.Content))
+            {
+                return new ServiceResult()
+                {
+                    Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                    {
+                        Title = exception.Message,
+                    },
+                    StatusCode = exception.StatusCode
+                };
+            }
+            else
+            {
+                var problemDetails = JsonSerializer.Deserialize<Microsoft.AspNetCore.Mvc.ProblemDetails>(exception.Content, new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true,
+                });
+
+
+                return new ServiceResult()
+                {
+                    Fail = problemDetails,
+                    StatusCode = exception.StatusCode
+                };
+            }
         }
 
 
@@ -45,7 +128,9 @@ namespace Microservice_Net9_.Shared
     public class ServiceResult<T> : ServiceResult
     {
         public T? Data { get; set; } //if it is not success
+        public string? UrlAsCreated { get; set; }
 
+        //200
         public static ServiceResult<T> SuccessAsOk(T data)
         {
             return new ServiceResult<T>()
@@ -53,6 +138,97 @@ namespace Microservice_Net9_.Shared
                 StatusCode = HttpStatusCode.OK,
                 Data = data
             };
+        }
+
+        //201 => Created => response body header => location => api/products/5
+        public static ServiceResult<T> SuccessAsCreated(T data, string url)
+        {
+            return new ServiceResult<T>()
+            {
+                StatusCode = HttpStatusCode.Created,
+                Data = data,
+                UrlAsCreated = url
+            };
+        }
+
+        public new static ServiceResult<T> Error(Microsoft.AspNetCore.Mvc.ProblemDetails problemDetails, HttpStatusCode statusCode)
+        {
+            return new ServiceResult<T>()
+{
+                StatusCode = statusCode,
+                Fail = problemDetails
+            };
+        }
+
+        public new static ServiceResult<T> Error(string title, string detail, HttpStatusCode statusCode)
+        {
+            return new ServiceResult<T>()
+            {
+                StatusCode = statusCode,
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                {
+                    Title = title,
+                    Detail = detail,
+                    Status = statusCode.GetHashCode() //int karşılığı
+                }
+            };
+        }
+
+        public new static ServiceResult<T> Error(string title, HttpStatusCode statusCode)
+        {
+            return new ServiceResult<T>()
+            {
+                StatusCode = statusCode,
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                {
+                    Title = title,
+                    Status = statusCode.GetHashCode() //int karşılığı
+                }
+            };
+        }
+
+        public new static ServiceResult<T> ErrorFromValidation(IDictionary<string, object?> errors)
+        {
+            return new ServiceResult<T>()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                {
+                    Title = "Validation errors occured.",
+                    Detail = "Please check the errors property for more details",
+                    Extensions = errors,
+                    Status = HttpStatusCode.BadRequest.GetHashCode() //int karşılığı
+                }
+            };
+        }
+
+        public new static ServiceResult<T> ErrorFromProblemDetails(ApiException exception)
+        {
+            if (string.IsNullOrEmpty(exception.Content))
+            {
+                return new ServiceResult<T>()
+                {
+                    Fail = new Microsoft.AspNetCore.Mvc.ProblemDetails()
+                    {
+                        Title = exception.Message,
+                    },
+                    StatusCode = exception.StatusCode
+                };
+            }
+            else
+            {
+                var problemDetails = JsonSerializer.Deserialize<Microsoft.AspNetCore.Mvc.ProblemDetails>(exception.Content, new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true,
+                });
+
+
+                return new ServiceResult<T>()
+                {
+                    Fail = problemDetails,
+                    StatusCode = exception.StatusCode
+                };
+            }
         }
     }
 
