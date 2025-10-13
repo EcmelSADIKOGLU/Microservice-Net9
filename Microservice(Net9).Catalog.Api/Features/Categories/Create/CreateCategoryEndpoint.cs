0429cf4c405e7f3a71d8fab8c3f5d5abf6d145f0
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2025-10-13 19:05:09 +0300

    Added: GenericResultExtention

--- Microservice(Net9).Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
+++ Microservice(Net9).Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microservice_Net9_.Shared.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Microservice_Net9_.Catalog.Api.Features.Categories.Create
@@ -7,16 +8,11 @@ namespace Microservice_Net9_.Catalog.Api.Features.Categories.Create
     {
         public static RouteGroupBuilder CreateCategoryGroupItem(this RouteGroupBuilder group)
         {
-            // http://localhost:5000/api/categories/
             group.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) =>
             {
 
                 var result = await mediator.Send(command);
-
-                return new ObjectResult(result)
-                {
-                    StatusCode = result.StatusCode.GetHashCode(),
-                };
+                return result.ToGenericResult();
 
             });
 
