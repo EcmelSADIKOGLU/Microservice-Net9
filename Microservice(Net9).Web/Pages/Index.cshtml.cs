31a89a5d2c8d3e349cd94871a009cf0963bea119
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2025-12-06 20:20:00 +0300

    Added: Razor Web Project

---
+++ Microservice(Net9).Web/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microservice_Net9_.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
