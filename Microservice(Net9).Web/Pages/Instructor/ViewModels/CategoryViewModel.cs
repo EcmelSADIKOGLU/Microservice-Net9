6069e413c822e13473b95f76c28f705c0c4d491d
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2025-12-09 22:24:38 +0300

    Add ViewModels for Course and Category management
    Introduced `CategoryViewModel` to represent course categories
    with `Id` and `Name` properties. Added `CourseViewModel` to
    represent course details, including a `TruncateDescription`
    method for truncating long descriptions.
    
    Created `CreateCourseViewModel` to support course creation,
    including properties for course details, a `CategoryDropdownList`
    for category selection, and a `SetCategoryDropdownList` method
    to populate the dropdown. Added data annotations for improved
    UI display and a static `Empty` instance for default initialization.

---
+++ Microservice(Net9).Web/Pages/Instructor/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,2 @@
+﻿namespace Microservice_Net9_.Web.Pages.Instructor.ViewModels;
+public record CategoryViewModel(Guid Id, string Name);
---
+++ Microservice(Net9).Web/Pages/Instructor/ViewModels/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Microservice_Net9_.Web.Pages.Instructor.ViewModels;
+public record CourseViewModel(
+    Guid Id,
+    string Name,
+    string Description,
+    decimal Price,
+    string ImageUrl,
+    string Created,
+    string EducatorFullName,
+    string CategoryName,
+    int Duration,
+    float Rating)
+{
+    public string TruncateDescription(int maxLength)
+    {
+        if (Description.Length <= maxLength) return Description;
+        return Description.Substring(0, maxLength) + "...";
+    }
+}
